d0063f1259ad78a42270d8ed6443d8017a70ebc9
Musab Gulfam <musabgulfam0722@gmail.com>
2022-01-23 21:43:20 +0500

    Added Async storage

--- src/screens/TaskListScreen.js
+++ src/screens/TaskListScreen.js
@@ -1,5 +1,6 @@
 import React, {
-    useState
+    useState,
+    useEffect
 } from 'react'
 import {
     View,
@@ -7,21 +8,37 @@ import {
     StyleSheet,
     TouchableOpacity,
     FlatList,
-    Image
+    Image,
+    TextInput
 } from 'react-native'
-import CheckBox from '@react-native-community/checkbox';
+import CheckBox from '@react-native-community/checkbox'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function TaskListScreen(props){
+export default function TaskListScreen(props) {
 
     const [tasks, setTasks] = useState([
-        {id: 0, activity: "Wake up", completed: false},
-        {id: 1, activity: "Pray", completed: false},
-        {id: 2, activity: "Excercise", completed: false},
-        {id: 3, activity: "Breakfast", completed: false},
-        {id: 4, activity: "Assignment", completed: false},
+        // {id: 0, activity: "Wake up", completed: false},
+        // {id: 1, activity: "Pray", completed: false},
+        // {id: 2, activity: "Excercise", completed: false},
+        // {id: 3, activity: "Breakfast", completed: false},
+        // {id: 4, activity: "Assignment", completed: false},
     ]);
 
-    const pushTask = (title, taskDiscription) => {
+    useEffect(_ => {
+        getTasksFromLS();
+    }, []);
+
+    const getTasksFromLS = async _ => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('tasks');
+            console.log('Array: ', jsonValue);
+            jsonValue != null ? setTasks(JSON.parse(jsonValue)) : null
+        } catch (e) {
+            // read error
+        }
+    }
+
+    const pushTask = async (title, taskDiscription) => {
         const allTasks = tasks;
         setTasks([...allTasks, {
             id: tasks.length,
@@ -29,22 +46,53 @@ export default function TaskListScreen(props){
             activity: taskDiscription,
             completed: false
         }]);
+
+        try {
+            const jsonValue = JSON.stringify([...allTasks, {
+                id: tasks.length,
+                title,
+                activity: taskDiscription,
+                completed: false
+            }])
+            await AsyncStorage.setItem('tasks', jsonValue)
+        } catch (e) {
+            // save error
+            console.error(e);
+        }
+        console.log('Done.')
     }
 
-    const deleteTask = id => {
+    const deleteTask = async id => {
         const updatedTasksList = tasks.filter(task => task.id !== id);
         setTasks([...updatedTasksList]);
+        try {
+            const jsonValue = JSON.stringify(updatedTasksList)
+            await AsyncStorage.setItem('tasks', jsonValue)
+        } catch (e) {
+            // save error
+            console.error(e);
+        }
+        console.log('Done.')
     }
+
+    const [searchText, setSearchText] = useState('');
 
     return (
         <View style={styles.contianer}>
-            { tasks.length > 0 ? <FlatList 
+            <View>
+                <TextInput
+                    value={searchText}
+                    onChangeText={text => setSearchText(text)}
+                />
+            </View>
+            {tasks.length > 0 ? <FlatList
                 data={tasks}
-                renderItem={({item, index}) => {
+                renderItem={({ item, index }) => {
                     return (
-                        
+
                         <View style={styles.taskItemContainer}>
                             <View>
+                                <Text style={styles.itemTitleStyle}>{item.title}</Text>
                                 <Text style={styles.taskDiscriptionStyle}>{item.activity}</Text>
                             </View>
                             <View
@@ -64,13 +112,13 @@ export default function TaskListScreen(props){
                                         onCheckColor="white"
                                     />
                                 </View>
-                                <TouchableOpacity 
+                                <TouchableOpacity
                                     style={styles.binView}
                                     onPress={_ => {
                                         deleteTask(item.id)
                                     }}
                                 >
-                                    <Image 
+                                    <Image
                                         source={require('../../assets/bin.png')}
                                         style={{
                                             width: 30,
@@ -84,8 +132,8 @@ export default function TaskListScreen(props){
                 }}
                 showsVerticalScrollIndicator={false}
             /> : <View style={styles.noTaskTextContainer}>
-                    <Text style={styles.noTaskTextStyle}>You are all done!</Text>
-                </View>}
+                <Text style={styles.noTaskTextStyle}>You are all done!</Text>
+            </View>}
             <View style={styles.addIconContainer}>
                 <TouchableOpacity
                     style={styles.buttonStyle}
@@ -95,7 +143,7 @@ export default function TaskListScreen(props){
                         }
                     })}
                 >
-                    <Image 
+                    <Image
                         source={require('../../assets/add.png')}
                         style={styles.addIconStyle}
                     />
@@ -114,7 +162,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         paddingVertical: 10
-    },  
+    },
     taskItemContainer: {
         paddingHorizontal: 20,
         borderRadius: 20,
@@ -168,9 +216,13 @@ const styles = StyleSheet.create({
     },
     secondaryContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        alignItems: 'center'
     },
     binView: {
         marginLeft: 20
+    },
+    itemTitleStyle: {
+        fontSize: 30
     }
 });
